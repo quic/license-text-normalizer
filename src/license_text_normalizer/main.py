2c3030275368b7942330d0502765604da743e829
Srinivasa Rao Kottala <srinivaskbs50@gmail.com>
2020-08-22 04:21:32 +0530

    Strip <BR> tag (#5)
    Signed-off-by: Srinivasa Rao Kottala <quic_skottala@quicinc.com>
    Signed-off-by: Jesse Porter <quic_jporter@quicinc.com>

--- src/license_text_normalizer/main.py
+++ src/license_text_normalizer/main.py
@@ -32,11 +32,7 @@ DEFAULT_BULLET_DELIMITERS: List[str] = ["*", "-"]
 
 DEFAULT_TRAILING_DELIMITERS: List[str] = ["*/", "*;", "*"]
 
-DEFAULT_WORDS_TO_STRIP: List[str] = [
-    "\0",  # null char
-    "echo",
-    "dnl",
-]
+DEFAULT_WORDS_TO_STRIP: List[str] = ["\0", "echo", "dnl", "<BR>"]
 
 
 def normalize_license_text(
